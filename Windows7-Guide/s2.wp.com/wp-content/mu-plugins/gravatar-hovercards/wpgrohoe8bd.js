be58d8d6ce06fa62f6c7e34b0c9fa4d60667c51a
Sabya Sachi Pal <98833669+r3ckl3ssr3v@users.noreply.github.com>
2024-06-01 20:20:14 +0530

    Update wpgrohoe8bd.js

--- Windows7-Guide/s2.wp.com/wp-content/mu-plugins/gravatar-hovercards/wpgrohoe8bd.js
+++ Windows7-Guide/s2.wp.com/wp-content/mu-plugins/gravatar-hovercards/wpgrohoe8bd.js
@@ -1,1 +1,38 @@
-(function(){var e=window.WPGroHo||{};e.my_hash="";e.data={};e.renderers={};e.syncProfileData=function(a,r){if(!e.data[a]){e.data[a]={};var n=document.querySelectorAll("div.grofile-hash-map-"+a+" span");for(var t=0;t<n.length;t++){var o=n[t];e.data[a][o.className]=o.textContent}}e.appendProfileData(e.data[a],a,r)};e.appendProfileData=function(a,r,n){for(var t in a){if(typeof e.renderers[t]==="function"){return e.renderers[t](a[t],r,n,t)}var o=document.getElementById(n);if(o){var d=o.querySelector("h4");if(d){var i=document.createElement("p");i.className="grav-extra "+t;i.innerHTML=a[t];d.insertAdjacentElement("afterend",i)}}}};window.WPGroHo=e})();
+(function() {
+    var WPGroHo = window.WPGroHo || {};
+    WPGroHo.my_hash = "";
+    WPGroHo.data = {};
+    WPGroHo.renderers = {};
+
+    WPGroHo.syncProfileData = function(userId, profileId) {
+        if (!WPGroHo.data[userId]) {
+            WPGroHo.data[userId] = {};
+            var elements = document.querySelectorAll("div.grofile-hash-map-" + userId + " span");
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                WPGroHo.data[userId][element.className] = element.textContent;
+            }
+        }
+        WPGroHo.appendProfileData(WPGroHo.data[userId], userId, profileId);
+    };
+
+    WPGroHo.appendProfileData = function(data, userId, profileId) {
+        for (var key in data) {
+            if (typeof WPGroHo.renderers[key] === "function") {
+                return WPGroHo.renderers[key](data[key], userId, profileId, key);
+            }
+            var targetElement = document.getElementById(profileId);
+            if (targetElement) {
+                var headingElement = targetElement.querySelector("h4");
+                if (headingElement) {
+                    var paragraphElement = document.createElement("p");
+                    paragraphElement.className = "grav-extra " + key;
+                    paragraphElement.innerHTML = data[key];
+                    headingElement.insertAdjacentElement("afterend", paragraphElement);
+                }
+            }
+        }
+    };
+
+    window.WPGroHo = WPGroHo;
+})();
